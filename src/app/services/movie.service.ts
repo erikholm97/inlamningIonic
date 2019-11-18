6ea91cd094b6dae440e0a8a5f3534617e7400cf0
erikholm97 <erikholm97@hotmail.com>
2019-11-18 09:46:00 +0100

    Added functions for movie app, routing and added elements in html

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+//import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  url = 'http://www.omdbapi.com/';
+  apiKey = 'e3afe514';
+
+  constructor(private http: HttpClient) { }
+// tslint:disable-next-line: no-unused-expression
+  searchData(title: string, type: SearchType): Observable < any > {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`)
+      .pipe(
+        map(results => {
+          return results['Search'];// Maps the results to something else 
+        })
+      );
+  }
+
+    getDetails(id){
+      return this.http.get(`${this.url}?=${id}&plot=full&apikey=${this.apiKey}`);
+    }
+  }
+
+
