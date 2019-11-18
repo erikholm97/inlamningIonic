cdd6c06b2e7b7aa97d00b7bbc5096f5c37fe4c9a
erikholm97 <erikholm97@hotmail.com>
2019-11-18 23:12:58 +0100

    routing solved

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MovieService } from 'src/app/services/movie.service';
+import { MovieService } from "./../../services/movie.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.page.html',
-  styleUrls: ['./movie-details.page.scss'],
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.page.html",
+  styleUrls: ["./movie-details.page.scss"]
 })
 export class MovieDetailsPage implements OnInit {
-
   information = null;
-  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService ) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
-    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    let id = this.activatedRoute.snapshot.paramMap.get("id");
+    console.log(id);
 
     this.movieService.getDetails(id).subscribe(result => {
-      console.log('details: ', result);
+      console.log("details: ", result);
       this.information = result;
     });
   }
-  openWebsite(){
-    window.open(this.information.Website, '_blanc');
+  openWebsite() {
+    window.open(this.information.Website, "_blank");
   }
 }
